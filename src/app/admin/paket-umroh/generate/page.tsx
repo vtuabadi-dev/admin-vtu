b79201b27592e507fdb2b0cc14065125bdf26187
Vtuabadi <vtuabadi@gmail.com>
2026-07-19 09:56:50 +0700

    feat: show route code (kode) in Rute In-Out select options on package generator page

--- src/app/admin/paket-umroh/generate/page.tsx
+++ src/app/admin/paket-umroh/generate/page.tsx
@@ -141,7 +141,7 @@ export default function GeneratePaketPage() {
               <select name="landingPatternId" value={formData.landingPatternId} onChange={handleChange} className="w-full h-10 px-3 py-2 rounded-md border border-input bg-transparent">
                 <option value="">-- Pilih Rute --</option>
                 {MOCK_LANDING_PATTERN.map((item) => (
-                  <option key={item.id} value={item.id}>{item.nama} ({item.rute})</option>
+                  <option key={item.id} value={item.id}>{item.kode} - {item.nama} ({item.rute})</option>
                 ))}
               </select>
             </div>
